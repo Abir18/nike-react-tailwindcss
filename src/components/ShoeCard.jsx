ffaeeccbc5824496eec72707fed0c855583f4bf8
Abir <rifatabir21@gmail.com>
2024-03-16 15:36:30 +0600

    responsive layout done

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,24 +1,25 @@
-const ShoeCard = ({
-  shoe,
-  bigShoeImage,
-  setBigShoeImage,
-  changeBigShoeImage
-}) => {
+const ShoeCard = ({imgURL, changeBigShoeImage, bigShoeImg}) => {
+  const handleClick = () => {
+    if (bigShoeImg !== imgURL.bigShoe) {
+      changeBigShoeImage(imgURL.bigShoe);
+    }
+  };
+
   return (
     <div
-      className={`border-2 rounded-xl cursor-pointer max-sm:flex-1 ${
-        bigShoeImage === shoe.bigShoe
+      className={`border-2 rounded-xl ${
+        bigShoeImg === imgURL.bigShoe
           ? "border-coral-red"
           : "border-transparent"
-      }`}
-      onClick={() => setBigShoeImage(shoe.bigShoe)}
+      } cursor-pointer max-sm:flex-1`}
+      onClick={handleClick}
     >
-      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:h-40 sm:w-40 rounded-xl max-sm:p-4">
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
         <img
-          src={shoe.thumbnail}
-          alt="shoe collection"
+          src={imgURL.thumbnail}
+          alt="shoe colletion"
           width={127}
-          height={103}
+          height={103.34}
           className="object-contain"
         />
       </div>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,22 +1,24 @@
 import {useState} from "react";
+
 import {arrowRight} from "../assets/icons";
-import {bigShoe2} from "../assets/images";
-import Button from "../components/Button";
-import ShoeCard from "../components/ShoeCard";
-import {shoes, statistics} from "./../constants/index";
+import {bigShoe1} from "../assets/images";
+import {Button, ShoeCard} from "../components";
+import {shoes, statistics} from "../constants";
 
 const Hero = () => {
-  const [bigShoeImage, setBigShoeImage] = useState(bigShoe2);
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
   return (
     <section
       id="home"
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">
-          Our Summer Collection
+          Our Summer collections
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
             The New Arrival
           </span>
@@ -27,10 +29,12 @@ const Hero = () => {
           Discover stylish Nike arrivals, quality comfort, and innovation for
           your active life.
         </p>
+
         <Button label="Shop now" iconURL={arrowRight} />
+
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
-          {statistics.map((stat) => (
-            <div key={stat.label}>
+          {statistics.map((stat, index) => (
+            <div key={index}>
               <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
               <p className="leading-7 font-montserrat text-slate-gray">
                 {stat.label}
@@ -39,22 +43,24 @@ const Hero = () => {
           ))}
         </div>
       </div>
-      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 py-40 bg-primary bg-hero bg-cover bg-center">
+
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img
-          src={bigShoeImage}
-          alt="shoe collection"
+          src={bigShoeImg}
+          alt="shoe colletion"
           width={610}
-          height={500}
+          height={502}
           className="object-contain relative z-10"
         />
 
-        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%]  max-sm:left-[10%] max-sm:px-6">
-          {shoes.map((shoe) => (
-            <div key={shoe.bigShoe}>
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((image, index) => (
+            <div key={index}>
               <ShoeCard
-                shoe={shoe}
-                bigShoeImage={bigShoeImage}
-                setBigShoeImage={setBigShoeImage}
+                index={index}
+                imgURL={image}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
               />
             </div>
           ))}
